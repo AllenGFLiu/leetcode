6b3718f16e1f8e60a441667d6a531449a988aa18
AllenGFLiu <allengfliu@foxmail.com>
2019-11-28 10:19:28 +0800

    检测链表是否有环的的实现

---
+++ #141-环形链表.py
@@ -0,0 +1,41 @@
+'''
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+示例 2：
+
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例 3：
+
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+'''
+
+from typing import Optional
+
+def hasCycle(self, head: ListNode) -> bool:
+    if not head or not head.next:
+        return False
+
+    slow = head
+    fast = head.next
+    while fast and fast.next:
+        if slow == fast:
+            return True
+        slow = slow.next
+        fast = fast.next.next
+    return True
