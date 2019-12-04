d2c06672853cbdfe16d1db7b3fed266e45fbbbe9
AllenGFLiu <allengfliu@foxmail.com>
2019-12-04 10:33:03 +0800

    新增Nim游戏的实现；新增BST最近公共祖先的实现

---
+++ #235-二叉搜索树的最近公共祖先.py
@@ -0,0 +1,40 @@
+'''
+给定一个二叉搜索树, 找到该树中两个指定节点的最近公共祖先。
+
+百度百科中最近公共祖先的定义为：“对于有根树 T 的两个结点 p、q，最近公共祖先表示为一个结点 x，满足 x 是 p、q 的祖先且 x 的深度尽可能大（一个节点也可以是它自己的祖先）。”
+
+例如，给定如下二叉搜索树:  root = [6,2,8,0,4,7,9,null,null,3,5]
+
+示例 1:
+
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 8
+输出: 6 
+解释: 节点 2 和节点 8 的最近公共祖先是 6。
+示例 2:
+
+输入: root = [6,2,8,0,4,7,9,null,null,3,5], p = 2, q = 4
+输出: 2
+解释: 节点 2 和节点 4 的最近公共祖先是 2, 因为根据定义最近公共祖先节点可以为节点本身。
+
+说明:
+
+所有节点的值都是唯一的。
+p、q 为不同节点且均存在于给定的二叉搜索树中。
+'''
+
+from typing import Optional
+
+
+# 利用二叉搜索树的特性:左子树的值都小于本节点，右子树的值都大于本节点
+# 判断给定的两个节点是当前root的左子树节点还是右子树节点，然后递归判定
+def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    root_val = root.val
+    p_val = p.val
+    q_val = q.val
+
+    if p_val > root_val and q_val > root_val:
+        return self.lowestCommonAncestor(root.right, p, q)
+    elif p_val < root_val and q_val < root_val:
+        return self.lowestCommonAncestor(root.left, p, q)
+    else:
+        return root
